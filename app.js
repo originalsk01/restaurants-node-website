9942c25883583bab792a1a97020b7ea06d2ce8f4
orginalsk01 <sipssman@gmail.com>
2023-05-01 18:25:03 +0200

    Move routes to separate folder

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const path = require("path");
-const fs = require("fs");
-const uuid = require("uuid");
 
 const express = require("express");
+
+const defaultRoutes = require("./routes/default");
+const restaurantRoutes = require("./routes/restaurant");
 
 const app = express();
 
@@ -15,56 +16,8 @@ app.use(express.static("public"));
 //Middleware to read form data
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", function (req, res) {
-  res.render("index");
-});
-
-app.get("/recommend", function (req, res) {
-  res.render("recommend");
-});
-
-app.get("/restaurants", function (req, res) {
-  const storedRestaurants = getStoredRestaurants();
-
-  res.render("restaurants", {
-    numberOfRestaurants: storedRestaurants.length,
-    restaurants: storedRestaurants,
-  });
-});
-
-app.get("/restaurants/:id", function (req, res) {
-  // restaurants/{uniqueId}
-  const restaurantId = req.params.id;
-  const storedRestaurants = getStoredRestaurants();
-
-  for (const restaurant of storedRestaurants) {
-    if (restaurant.id === restaurantId) {
-      return res.render("restaurant-details", { restaurant: restaurant });
-    }
-  }
-
-  res.status(404).render("404");
-});
-
-app.post("/recommend", function (req, res) {
-  const restaurant = req.body;
-  restaurant.id = uuid.v4(); // Add unique id to the restaurant
-  const storedRestaurants = getStoredRestaurants();
-
-  storedRestaurants.push(restaurant);
-
-  storeRestaurants(storedRestaurants);
-
-  res.redirect("/confirm");
-});
-
-app.get("/confirm", function (req, res) {
-  res.render("confirm");
-});
-
-app.get("/about", function (req, res) {
-  res.render("about");
-});
+app.use("/", defaultRoutes);
+app.use("/", restaurantRoutes);
 
 app.use(function (req, res) {
   res.status(404).render("404");
